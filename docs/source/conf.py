a48f2bf4e125ee9302e242476c7b07b5d8db1ea8
ali-haidar <a.haidar@unsw.edu.au>
2020-03-19 10:49:40 +1100

    version 0.0.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Ali Haidar'
 author = 'Ali Haidar'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.5'
+release = '0.0.7'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     name='pspso',
     license="MIT",
     description='pspso is a python package for selecting machine learning algorithms parameters.',
-    version='v0.0.6',
+    version='v0.0.7',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/ayhaidar/pspso',
