246b4bcee8896f0a9f2ff7a8a65b850631aaebcd
ali-haidar <a.haidar@unsw.edu.au>
2020-06-03 13:13:55 +1000

    added tensorflow to requirements.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Ali Haidar'
 author = 'Ali Haidar'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ setuptools.setup(
     name='pspso',
     license="MIT",
     description='pspso is a python package for selecting machine learning algorithms parameters.',
-    version='v0.1.0',
+    version='v0.1.1',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/ayhaidar/pspso',
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
-    install_requires=['numpy==1.16.1','lightgbm','xgboost','scikit-learn>=0.21.2','keras','pyswarms>=1.0.2','matplotlib>=3.1.1'],
+    install_requires=['numpy==1.16.1','lightgbm','xgboost','scikit-learn>=0.21.2','keras','tensorflow','pyswarms>=1.0.2','matplotlib>=3.1.1'],
     classifiers=[
         # Trove classifiers
         # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
